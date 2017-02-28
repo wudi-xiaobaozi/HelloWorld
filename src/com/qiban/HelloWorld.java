d1fc6d245a3fccdaafa504a986478bee7ee1e469
wudi-xiaobaozi <18310673895163.com>
2017-02-28 10:54:15 +0800

    删除了hello2

--- src/com/qiban/HelloWorld.java
+++ src/com/qiban/HelloWorld.java
@@ -5,7 +5,6 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
          System.out.println("HelloWorld");
-		 System.out.println("HelloWorld2");
 		 System.out.println("HelloWorld.main()");
 		
 		
