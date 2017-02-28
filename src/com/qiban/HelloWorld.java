69c7913eb74855248cc7d8b33d66c4ed94c15751
wudi-xiaobaozi <18310673895163.com>
2017-02-28 10:55:57 +0800

    删除了所有的Hello

--- src/com/qiban/HelloWorld.java
+++ src/com/qiban/HelloWorld.java
@@ -4,7 +4,6 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-         System.out.println("HelloWorld");
 		 System.out.println("HelloWorld.main()");
 		
 		
